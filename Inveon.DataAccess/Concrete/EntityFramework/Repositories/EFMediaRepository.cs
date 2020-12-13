911185c41ae532225d1dae6c6b44112f6f903d2b
frknlkn <furkanalkan9494@gmail.com>
2020-12-14 02:45:55 +0300

    Fixed media repository bug

---
+++ Inveon.DataAccess/Concrete/EntityFramework/Repositories/EFMediaRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Inveon.DataAccess.Interfaces;
+
+namespace Inveon.DataAccess.Concrete.EntityFramework.Repositories
+{
+    public class EFMediaRepository: Repository<Media>, IMediaRepository
+    {
+        public EFMediaRepository(DbContext context) : base(context)
+        {
+        }
+
+        public List<Media> GetProductImages(int productId)
+        {
+            return FindBy(x => x.ProductId == productId).ToList();
+        }
+    }
+}
